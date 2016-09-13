17b181fb01adcc440b736b65f090e1223e345f03
Ken Johnson <ken.m.johnson@hotmail.com>
2016-09-13 11:50:55 -0500

    Create pli.js

---
+++ servers/pli.js
@@ -0,0 +1,50 @@
+
+module.exports = {
+
+  checkPLI: function (io) {
+    var result = '';
+
+    var sqlPLI = require("mssql");
+    var conPLI = {
+        
+        // SQL Server string here
+        
+        options: {
+          tdsVersion: '7_1'
+        }
+    };
+
+
+    var con = new sqlPLI.Connection(conPLI, function (err) {
+      if (err) {
+        console.log(err);
+        result = 'Error connecting to PLIAPPS database';
+        require('../sendmsg').sendText(result);
+      }
+
+      // create Request object
+      var reqPLI = new sqlPLI.Request(con);
+
+      // query to the database and get the records
+      reqPLI.query('SELECT LastDate FROM tblLastDate', function (err, recordset) {
+
+        if (err) {
+          console.log(err);
+          result = 'Error returning data from PLIAPPS database';
+          require('../sendmsg').sendText(result);
+        }
+
+
+
+        result = recordset[0].LastDate.toString();
+        console.log('pli.js line 48: ' + result );
+
+        io.emit('chat message', result);
+
+      });
+  });
+
+
+  }
+
+};
