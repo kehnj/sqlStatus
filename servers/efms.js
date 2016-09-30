865bc771e9b23eff995fe6c4b28c724dac6da7b5
Ken Johnson <ken.m.johnson@hotmail.com>
2016-09-30 12:16:01 -0500

    Add files via upload

--- servers/efms.js
+++ servers/efms.js
@@ -1,44 +1,49 @@
-module.exports = {
+const SendMsg = require('../utilities/sendmsg');
 
-  checkEFMS: function (io) {
-    var result = '';
+function checkSQL(io) {
+  var result = '';
 
-    var sqlEFMS = require("mssql");
-    var conEFMS = {
+  var sqlEFMS = require("mssql");
+  var conEFMS = {
+    user: process.env.sqlUser,
+    password:  process.env.sqlPassword,
+    server:  process.env.efmsServer,
+    database:  process.env.efmsDatabase,
+      options: {
+        tdsVersion: '7_1'
+      }
+  };
 
-        // SQL Server connection string here
+  var con = new sqlEFMS.Connection(conEFMS, function (err) {
+    if (err) {
+      console.log(err);
+      result = 'Error connecting to EFMS2 database';
 
-        options: {
-          tdsVersion: '7_1'
-        }
-    };
+      //todo.. why not put more of the err message in the txt message?
+      SendMsg.sendText(result);
+    }
 
+    // create Request object
+    var reqEFMS = new sqlEFMS.Request(con);
 
-    var con = new sqlEFMS.Connection(conEFMS, function (err) {
+    // query to the database and get the records
+    reqEFMS.query('SELECT MAX(AuditDate) AS LastDate FROM AuditLog', function (err, recordset) {
+
       if (err) {
-        console.log(err);
-        result = 'Error connecting to EFMS2 database';
-        require('../sendmsg').sendText(result);
+        console.log(err)
+        result = 'Error returning data from EFMS2 database';
+        SendMsg.sendText(result);
+        io.emit('sqlStatus', 'efms', 'NO RESULTS', '---', new Date().toLocaleTimeString());
       }
 
-      // create Request object
-      var reqEFMS = new sqlEFMS.Request(con);
+      result = recordset[0].LastDate.toLocaleDateString();
 
-      // query to the database and get the records
-      reqEFMS.query('SELECT MAX(AuditDate) AS LastDate FROM AuditLog', function (err, recordset) {
+      io.emit('sqlStatus', 'efms', 'OK', result, new Date().toLocaleTimeString());
 
-        if (err) {
-          console.log(err)
-          result = 'Error returning data from EFMS2 database';
-          require('../sendmsg').sendText(result);
-        }
-
-        result = recordset[0].LastDate.toString();
-        io.emit('chat message', result);
-
-      });
     });
+  });
+};
 
-  }
-
+exports.checkSQL = function (io) {
+  setInterval(checkSQL, 5000, io);
 };
--- servers/pli.js
+++ servers/pli.js
@@ -1,48 +1,47 @@
+const SendMsg = require('../utilities/sendmsg');
 
-module.exports = {
+function checkSQL(io){
+  var result = '';
 
-  checkPLI: function (io) {
-    var result = '';
+  var sqlPLI = require("mssql");
+  var conPLI = {
+    user: process.env.sqlUser,
+    password:  process.env.sqlPassword,
+    server:  process.env.pliServer,
+    database:  process.env.pliDatabase,
+    options: {
+      tdsVersion: '7_1'
+    }
+  };
 
-    var sqlPLI = require("mssql");
-    var conPLI = {
-        
-        // SQL Server string here
-        
-        options: {
-          tdsVersion: '7_1'
-        }
-    };
+  var con = new sqlPLI.Connection(conPLI, function (err) {
+    if (err) {
+      console.log(err);
+      result = 'Error connecting to PLIAPPS database';
 
+      //todo.. why not put more of the err message in the txt message?
+      SendMsg.sendText(result);
+    }
 
-    var con = new sqlPLI.Connection(conPLI, function (err) {
+    // create Request object
+    var reqPLI = new sqlPLI.Request(con);
+
+    // query to the database and get the records
+    reqPLI.query('SELECT LastDate3 FROM tblLastDate', function (err, recordset) {
+
       if (err) {
         console.log(err);
-        result = 'Error connecting to PLIAPPS database';
-        require('../sendmsg').sendText(result);
+        result = 'Error returning data from PLIAPPS database';
+        SendMsg.sendText(result);
+        io.emit('sqlStatus', 'pli-app', 'NO RESULTS', '---', new Date().toLocaleTimeString());
+      } else {
+        result = recordset[0].LastDate.toLocaleDateString();
+        io.emit('sqlStatus', 'pli-app', 'OK', result, new Date().toLocaleTimeString());
       }
-
-      // create Request object
-      var reqPLI = new sqlPLI.Request(con);
-
-      // query to the database and get the records
-      reqPLI.query('SELECT LastDate FROM tblLastDate', function (err, recordset) {
-
-        if (err) {
-          console.log(err);
-          result = 'Error returning data from PLIAPPS database';
-          require('../sendmsg').sendText(result);
-        }
-
-
-
-        result = recordset[0].LastDate.toString();
-        io.emit('chat message', result);
-
-      });
+    });
   });
+};
 
-
-  }
-
+exports.checkSQL = function (io) {
+  setInterval(checkSQL, 5000, io);
 };
